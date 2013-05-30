6511145d1debad274694f70675788a74b3e907d1
Luke Leighton <lkcl@lkcl.net>
2013-05-30 20:05:17 +0100

    sunxi: Allwinner SUN7I (A20) support
    add a20 dram setup
    
    Supply para->tpr3 as phase argument to dll0 and dllx setup
    
    create CONFIG_SUN7I
    
    SUN7I is using the same MMC controller as SUN5I
    
    Signed-off-by: Luke Leighton <lkcl@lkcl.net>
    Signed-off-by: Henrik Nordstrom <henrik@henriknordstrom.net>
    Signed-off-by: Oliver Schinagl <oliver@schinagl.nl>

--- arch/arm/cpu/armv7/sunxi/dram.c
+++ arch/arm/cpu/armv7/sunxi/dram.c
@@ -1,6 +1,7 @@
 /*
  * sunxi DRAM controller initialization
  * (C) Copyright 2012 Henrik Nordstrom <henrik@henriknordstrom.net>
+ * (C) Copyright 2013 Luke Kenneth Casson Leighton <lkcl@lkcl.net>
  *
  * Based on sun4i Linux kernel sources mach-sunxi/pm/standby/dram*.c
  * and earlier U-Boot Allwiner A10 SPL work
@@ -75,7 +76,7 @@ static void mctl_itm_disable(void)
 {
 	struct sunxi_dram_reg *dram = (struct sunxi_dram_reg *)SUNXI_DRAMC_BASE;
 
-	setbits_le32(&dram->ccr, 0x1 << 28);
+	clrsetbits_le32(&dram->ccr, 0x1 << 31, 0x1 << 28);
 }
 
 static void mctl_itm_enable(void)
@@ -85,10 +86,12 @@ static void mctl_itm_enable(void)
 	clrbits_le32(&dram->ccr, 0x1 << 28);
 }
 
-static void mctl_enable_dll0(void)
+static void mctl_enable_dll0(u32 phase)
 {
 	struct sunxi_dram_reg *dram = (struct sunxi_dram_reg *)SUNXI_DRAMC_BASE;
 
+	clrsetbits_le32(&dram->dllcr[0], 0x3f << 6,
+			((phase >> 16) & 0x3f) << 6);
 	clrsetbits_le32(&dram->dllcr[0], 0x1 << 30, 0x1 << 31);
 	sdelay(0x100);
 
@@ -102,7 +105,7 @@ static void mctl_enable_dll0(void)
 /*
  * Note: This differs from pm/standby in that it checks the bus width
  */
-static void mctl_enable_dllx(void)
+static void mctl_enable_dllx(u32 phase)
 {
 	struct sunxi_dram_reg *dram = (struct sunxi_dram_reg *)SUNXI_DRAMC_BASE;
 	u32 i, n, bus_width;
@@ -117,7 +120,10 @@ static void mctl_enable_dllx(void)
 		n = 3;
 
 	for (i = 1; i < n; i++) {
+		clrsetbits_le32(&dram->dllcr[i], 0x4 << 14,
+				(phase & 0xf) << 14);
 		clrsetbits_le32(&dram->dllcr[i], 0x1 << 30, 0x1 << 31);
+		phase >>= 4;
 	}
 	sdelay(0x100);
 
@@ -150,6 +156,16 @@ static u32 hpcr_value[32] = {
 	0x1035, 0x0731, 0x1031, 0x0735,
 	0x1035, 0x1031, 0x0731, 0x1035,
 	0x1031, 0x0301, 0x0301, 0x0731
+#endif
+#ifdef CONFIG_SUN7I
+	0x0301, 0x0301, 0x0301, 0x0301,
+	0x0301, 0x0301, 0x0301, 0x0301,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0x1031, 0x1031, 0x0735, 0x1035,
+	0x1035, 0x0731, 0x1031, 0x0735,
+	0x1035, 0x1031, 0x0731, 0x1035,
+	0x0001, 0x1031, 0x0000, 0x0731
 #endif
 };
 
@@ -184,7 +200,7 @@ static void mctl_setup_dram_clock(u32 clk)
 
 	setbits_le32(&ccm->pll5_cfg, 0x1 << 29);
 
-#ifdef CONFIG_SUN4I
+#if defined(CONFIG_SUN4I) || defined(CONFIG_SUN7I)
 	/* reset GPS */
 	clrbits_le32(&ccm->gps_clk_cfg, 0x3);
 	setbits_le32(&ccm->ahb_gate0, 0x1 << 26);
@@ -200,7 +216,7 @@ static void mctl_setup_dram_clock(u32 clk)
 	 * open DRAMC AHB & DLL register clock
 	 * close it first
 	 */
-#ifdef CONFIG_SUN5I
+#if defined(CONFIG_SUN5I) || defined(CONFIG_SUN7I)
 	clrbits_le32(&ccm->ahb_gate0, 0x3 << 14);
 #else
 	clrbits_le32(&ccm->ahb_gate0, 0x1 << 14);
@@ -208,7 +224,7 @@ static void mctl_setup_dram_clock(u32 clk)
 	sdelay(0x1000);
 
 	/* then open it */
-#ifdef CONFIG_SUN5I
+#if defined(CONFIG_SUN5I) || defined(CONFIG_SUN7I)
 	setbits_le32(&ccm->ahb_gate0, 0x3 << 14);
 #else
 	setbits_le32(&ccm->ahb_gate0, 0x1 << 14);
@@ -235,9 +251,106 @@ static int dramc_scan_readpipe(void)
 	return 0;
 }
 
+static int dramc_scan_dll_para(void)
+{
+	struct sunxi_dram_reg *dram = (struct sunxi_dram_reg *)SUNXI_DRAMC_BASE;
+	const u32 dqs_dly[7] = {0x3, 0x2, 0x1, 0x0, 0xe, 0xd, 0xc};
+	const u32 clk_dly[15] = {0x07, 0x06, 0x05, 0x04, 0x03,
+				 0x02, 0x01, 0x00, 0x08, 0x10,
+				 0x18, 0x20, 0x28, 0x30, 0x38};
+	u32 clk_dqs_count[15];
+	u32 dqs_i, clk_i, cr_i;
+	u32 max_val, min_val;
+	u32 dqs_index, clk_index;
+
+	for (clk_i = 0; clk_i < 15; clk_i++) {
+		clk_dqs_count[clk_i] = 0;
+		clrsetbits_le32(&dram->dllcr[0], 0x3f << 6,
+				(clk_dly[clk_i] & 0x3f) << 6);
+		for (dqs_i = 0; dqs_i < 7; dqs_i++) {
+			for (cr_i = 1; cr_i < 5; cr_i++) {
+				clrsetbits_le32(&dram->dllcr[cr_i],
+						0x4f << 14,
+						(dqs_dly[clk_i] & 0x4f) << 14);
+			}
+			sdelay(0x100);
+			if (dramc_scan_readpipe() == 0)
+				clk_dqs_count[clk_i]++;
+		}
+	}
+	for (dqs_i = 15; dqs_i > 0; dqs_i--) {
+		max_val = 15;
+		min_val = 15;
+		for (clk_i = 0; clk_i < 15; clk_i++) {
+			if (clk_dqs_count[clk_i] == dqs_i) {
+				max_val = clk_i;
+				if (min_val == 15)
+					min_val = clk_i;
+			}
+		}
+		if (max_val < 15)
+			break;
+	}
+	if (!dqs_i)
+		goto fail;
+
+	clk_index = (max_val + min_val) >> 1;
+	if ((max_val == (15 - 1)) && (min_val > 0))
+		clk_index = (15 + clk_index) >> 1;
+	else if ((max_val < (15 - 1)) && (min_val == 0))
+		clk_index >>= 1;
+	if (clk_dqs_count[clk_index] < dqs_i)
+		clk_index = min_val;
+
+	clrsetbits_le32(&dram->dllcr[0], 0x3f << 6,
+			(clk_dly[clk_index] & 0x3f) << 6);
+	max_val = 7;
+	min_val = 7;
+	for (dqs_i = 0; dqs_i < 7; dqs_i++) {
+		clk_dqs_count[dqs_i] = 0;
+		for (cr_i = 1; cr_i < 5; cr_i++) {
+			clrsetbits_le32(&dram->dllcr[cr_i],
+					0x4f << 14,
+					(dqs_dly[dqs_i] & 0x4f) << 14);
+		}
+		sdelay(0x100);
+		if (dramc_scan_readpipe() == 0) {
+			clk_dqs_count[dqs_i] = 1;
+			max_val = dqs_i;
+			if (min_val == 7)
+				min_val = dqs_i;
+		}
+	}
+
+	if (max_val < 7) {
+		dqs_index = (max_val + min_val) >> 1;
+		if ((max_val == (7-1)) && (min_val > 0))
+			dqs_index = (7 + dqs_index) >> 1;
+		else if ((max_val < (7-1)) && (min_val == 0))
+			dqs_index >>= 1;
+		if (!clk_dqs_count[dqs_index])
+			dqs_index = min_val;
+		for (cr_i = 1; cr_i < 5; cr_i++) {
+			clrsetbits_le32(&dram->dllcr[cr_i],
+					0x4f << 14,
+					(dqs_dly[dqs_index] & 0x4f) << 14);
+		}
+		sdelay(0x100);
+		return dramc_scan_readpipe();
+	}
+
+fail:
+	clrbits_le32(&dram->dllcr[0], 0x3f << 6);
+	for (cr_i = 1; cr_i < 5; cr_i++)
+		clrbits_le32(&dram->dllcr[cr_i], 0x4f << 14);
+	sdelay(0x100);
+
+	return dramc_scan_readpipe();
+}
+
 static void dramc_clock_output_en(u32 on)
 {
-#ifdef CONFIG_SUN5I
+#if defined(CONFIG_SUN5I) || defined(CONFIG_SUN7I)
 	struct sunxi_dram_reg *dram = (struct sunxi_dram_reg *)SUNXI_DRAMC_BASE;
 
 	if (on)
@@ -284,7 +397,7 @@ static void dramc_set_autorefresh_cycle(u32 clk)
 }
 #endif /* SUN4I */
 
-#ifdef CONFIG_SUN5I
+#if defined(CONFIG_SUN5I) || defined(CONFIG_SUN7I)
 static void dramc_set_autorefresh_cycle(u32 clk)
 {
 	struct sunxi_dram_reg *dram = (struct sunxi_dram_reg *)SUNXI_DRAMC_BASE;
@@ -420,7 +533,9 @@ int dramc_init(struct dram_para *para)
 #endif
 
 	/* reset external DRAM */
+#ifndef CONFIG_SUN7I
 	mctl_ddr3_reset();
+#endif
 	mctl_set_drive();
 
 	/* dram clock off */
@@ -432,7 +547,7 @@ int dramc_init(struct dram_para *para)
 #endif
 
 	mctl_itm_disable();
-	mctl_enable_dll0();
+	mctl_enable_dll0(para->tpr3);
 
 	/* configure external DRAM */
 	reg_val = 0;
@@ -454,17 +569,17 @@ int dramc_init(struct dram_para *para)
 		reg_val |= 0x5 << 3;
 	else
 		reg_val |= 0x0 << 3;
-
 	reg_val |= ((para->bus_width >> 3) - 1) << 6;
-
 	reg_val |= (para->rank_num - 1) << 10;
-
 	reg_val |= 0x1 << 12;
 	reg_val |= ((0x1) & 0x3) << 13;
-
 	writel(reg_val, &dram->dcr);
 
-#ifdef CONFIG_SUN5I
+#ifdef CONFIG_SUN7I
+	setbits_le32(&dram->zqcr1, 0x1 << 24);
+#endif
+
+#if (defined(CONFIG_SUN5I) || defined(CONFIG_SUN7I))
 	/* set odt impendance divide ratio */
 	reg_val = ((para->zq) >> 8) & 0xfffff;
 	reg_val |= ((para->zq) & 0xff) << 20;
@@ -472,14 +587,24 @@ int dramc_init(struct dram_para *para)
 	writel(reg_val, &dram->zqcr0);
 #endif
 
+#ifdef CONFIG_SUN7I
+	setbits_le32(&dram->idcr, 0x1ffff);
+#endif
+
 	/* dram clock on */
 	dramc_clock_output_en(1);
+#ifdef CONFIG_SUN7I
+	if ((readl(&dram->ppwrsctl) & 0x1) != 0x1)
+		mctl_ddr3_reset();
+	else
+		setbits_le32(&dram->mcr, 0x1 << 12);
+#endif
 
 	sdelay(0x10);
 
 	while (readl(&dram->ccr) & (0x1 << 31));
 
-	mctl_enable_dllx();
+	mctl_enable_dllx(para->tpr3);
 
 #ifdef CONFIG_SUN4I
 	/* set odt impendance divide ratio */
@@ -509,7 +634,7 @@ int dramc_init(struct dram_para *para)
 	if (para->type == 3) {
 		/* ddr3 */
 		reg_val = 0x0;
-#ifdef CONFIG_SUN5I
+#if (defined(CONFIG_SUN5I) || defined(CONFIG_SUN7I))
 		reg_val |= 0x1000;
 #endif
 		reg_val |= (para->cas - 4) << 4;
@@ -536,7 +661,19 @@ int dramc_init(struct dram_para *para)
 
 	/* scan read pipe value */
 	mctl_itm_enable();
-	ret_val = dramc_scan_readpipe();
+	if (para->tpr3 & (0x1 << 31)) {
+		ret_val = dramc_scan_dll_para();
+		if (ret_val == 0)
+			para->tpr3 =
+				(((readl(&dram->dllcr[0]) >> 6) & 0x3f) << 16) |
+				(((readl(&dram->dllcr[1]) >> 14) & 0xf) << 0) |
+				(((readl(&dram->dllcr[2]) >> 14) & 0xf) << 4) |
+				(((readl(&dram->dllcr[3]) >> 14) & 0xf) << 8) |
+				(((readl(&dram->dllcr[4]) >> 14) & 0xf) << 12
+				);
+	} else {
+		ret_val = dramc_scan_readpipe();
+	}
 
 	if (ret_val < 0)
 		return 0;
